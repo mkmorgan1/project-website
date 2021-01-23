5eb560c24e6a6d4b88b3f1ed0e427902ad2d2ae7
Matthew <m_kmorgan@hotmail.com>
2021-01-23 10:53:30 -0700

    Finished horizontal Scaling page

--- client/components/projects/HorizontallyScaling.jsx
+++ client/components/projects/HorizontallyScaling.jsx
@@ -6,26 +6,29 @@ const HorizontallyScaling = () => {
     <div className={style.videoAndDescription}>
 
       <div className={style.description}>
-        <h3>Horizontally Scaling of an Application</h3>
-        <p>User is a time traveler who shoots portals between the past and future to avoid obstacles.</p>
+        <h3 className={style.descriptionHeader}>Horizontal Scaling of an Application</h3>
+        <p>Loaded 10,000,000 records into a Neo4j and PostgreSQL database and horozontaly scaled the application to handle 20 times more requests per second</p>
         <ul>
           <li>
-            Utilized <b>​Webpack</b> ​to bundle all ​<b>Phaser 3</b> ​and ​<b>React</b>​ elements and an ​<b>Express​</b> server to render the game as a single page application
+            Testing locally with postman, <b>Neo4j</b> was faster than <b>PostgreSQL</b>
           </li>
           <li>
-            Applied ​<b>JavaScript Class Inheritance​</b> to create multiple levels with <b>reusable code</b>
+            Utilized <b>Axilary.io</b> locally to stress-test and compared the results with <b>NewRelic</b>
           </li>
           <li>
-            Includes a <b>tutorial scene</b> and a <b>credits scene</b> created with the user experience in mind
+            Launched and Implemented multiple ​<b>nginx​ ​ec2​ load-balancer</b> instances to direct traffic to ​<b>ec2</b>​ copies of the application
           </li>
           <li>
-            Created all the <b>art</b> and <b>animations</b>
+            Stress-Tested launched applicaiton with <b>Loader.io</b> while cross-refrencing <b>NewRelic</b>
+          </li>
+          <li>
+            Horizontally scaled the application to handle ​<b>1000 requests per second​</b> to average at <b>806 ms</b> per response
           </li>
         </ul>
       </div>
 
       <div className={style.videoBox}>
-        a picture
+        <img src="https://matthew-resume-website.s3.us-east-2.amazonaws.com/Screen+Shot+2020-12-18+at+8.18.29+PM.png" alt="Horozantal scaling"/>
         {/* <video autoPlay muted loop id='myVideo'>
           <source src='https://matthew-resume-website.s3.us-east-2.amazonaws.com/timeTravelAgent.mov' type='video/mp4'/>
           Your browser does not support HTML5 video.
