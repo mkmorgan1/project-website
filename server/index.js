f51bb0086298608b6cb85cab9545e8a6cc66c090
Matthew <m_kmorgan@hotmail.com>
2021-01-27 08:45:11 -0700

    Updated server for deployed ssl

--- server/index.js
+++ server/index.js
@@ -9,3 +9,26 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
+
+/* DEPLOYMENT SERVER SSL
+
+import express from 'express';
+import cors from 'cors';
+import https from 'https';
+import http from 'http';
+import path from 'path';
+import fs from 'fs';
+
+// create new express app and save it as "app"
+const app = express();
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Listen both http & https ports
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer({
+  key: fs.readFileSync('/etc/letsencrypt/live/www.matthewkerrymorgan.com/privkey.pem'), //'./generated-private-key.pem')),
+  cert: fs.readFileSync('/etc/letsencrypt/live/www.matthewkerrymorgan.com/fullchain.pem'), //'./generated-csr.pem')),
+}, app);
+
+*/
