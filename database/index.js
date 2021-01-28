39c12a77c73c188dd2a76c2f8c2c06b3b8d3b5bc
Matthew <m_kmorgan@hotmail.com>
2021-01-28 16:37:13 -0700

    created a get and post function fro postgres

---
+++ database/index.js
@@ -0,0 +1,30 @@
+import { Client, Pool } from 'pg';
+
+const postgres = new Client({
+  host: 'localhost',
+  user: 'matt',
+  database: 'related_products'
+});
+postgres.connect();
+
+export const postPostgres = (date, link, callback) => {
+  const postInteraction = `INSERT into interaction (date, link) VALUES ($1, $2)`
+  postgres.query(postInteraction, [date, link],(err, res) => {
+    if (err) {
+      callback('err');
+    } else {
+      callback(null, res);
+    }
+  })
+}
+
+export const getPostgres = (callback) => {
+  const getInteraction = `SELECT * FROM interaction`
+  postgres.query(getInteraction, (err, res) => {
+    if (err) {
+      callback('err');
+    } else {
+      callback(null, res);
+    }
+  })
+}
