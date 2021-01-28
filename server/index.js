4ed542d92ee6de4faa08376729f3351becc21812
Matthew <m_kmorgan@hotmail.com>
2021-01-28 13:48:33 -0700

    created a get and post in the server

--- server/index.js
+++ server/index.js
@@ -1,16 +1,3 @@
-import express from 'express';
-import path from 'path';
-
-const app = express();
-const PORT = 3030;
-
-app.use(express.static(path.join(__dirname, '../public')));
-
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
-});
-
-/* DEPLOYMENT SERVER SSL
 
 import express from 'express';
 import cors from 'cors';
@@ -24,11 +11,31 @@ const app = express();
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.get('/data', (req,res) => {
+  res.send('data get');
+});
+app.post('/data', (req, res) => {
+  res.send('data post');
+})
+
 // Listen both http & https ports
 const httpServer = http.createServer(app);
+
+/*
 const httpsServer = https.createServer({
   key: fs.readFileSync('/etc/letsencrypt/live/www.matthewkerrymorgan.com/privkey.pem'), //'./generated-private-key.pem')),
   cert: fs.readFileSync('/etc/letsencrypt/live/www.matthewkerrymorgan.com/fullchain.pem'), //'./generated-csr.pem')),
 }, app);
 
+httpsServer.listen(443, () => {
+  console.log('HTTPS Server running on port 443');
+});
+
 */
+
+httpServer.listen(80, () => {
+  console.log('HTTP Server running on port 80');
+});
+
+
+
