ecb02ae0f241554db04e293dfa0d2aec8566cb90
Matthew <m_kmorgan@hotmail.com>
2021-01-28 16:09:19 -0700

    created a postgres SQL file for tables

--- client/components/Functions.jsx
+++ client/components/Functions.jsx
@@ -5,10 +5,6 @@ export const clicked = (value) => {
     type: "POST",
     url: `/data?link=${value}`,
     data: 'hello',
-    success: () => {
-      console.log('sucess');
-    },
     dataType: 'text'
   });
-  console.log('here');
 }
--- server/database/postgres.sql
+++ server/database/postgres.sql
@@ -4,7 +4,13 @@ CREATE DATABASE website_data;
 \c related_products
 
 CREATE TABLE interaction (
+  date VARCHAR (30) NOT NULL,
+  link VARCHAR (30) NOT NULL
+);
 
+CREATE TABLE stats (
+  link VARCHAR (30) NOT NULL,
+  count VARCHAR smallint
 );
 
 -- psql postgres < database/postgres.sql
