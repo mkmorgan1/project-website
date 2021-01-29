a30df945979a4a25485ea52c231480169e9eabd3
Matthew <m_kmorgan@hotmail.com>
2021-01-28 17:52:10 -0700

    added a password auth for postgres

--- database/index.js
+++ database/index.js
@@ -3,6 +3,7 @@ import { Client, Pool } from 'pg';
 const postgres = new Client({
   host: process.env.PG_HOST,//'localhost',
   user: process.env.PG_USER,//'matt',
+  password: process.env.PG_PASSWORD,
   database: 'website_data'
 });
 postgres.connect();
