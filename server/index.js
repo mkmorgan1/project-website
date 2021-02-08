4b8c4f83145e93718252ee08a104ee646f15be84
Matthew <m_kmorgan@hotmail.com>
2021-02-08 14:15:15 -0700

    Updated delete call

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-// import cors from 'cors';
 // import httpolyglot from 'httpolyglot';
 import https from 'https';
 import http from 'http';
@@ -10,7 +9,6 @@ import { getPostgres, postPostgres, deletePostgres } from '../database/index.js'
 
 // create new express app and save it as "app"
 const app = express();
-// app.use(cors());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -45,29 +43,21 @@ app.delete('/data', (req, res) => {
       console.log(err);
       res.status(404).send(err);
     } else {
-      res.status(200).end();
+      res.status(200).send(`DELETED ${result.rowCount} rows`);
     }
   })
 })
 
 // Listen both http & https ports
+http.createServer(app).listen(80, () => console.log('HTTP Server running on port 80'));
+
+/*
 const options = {
   key: fs.readFileSync('/etc/letsencrypt/live/www.matthewkerrymorgan.com/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/www.matthewkerrymorgan.com/fullchain.pem'),
 }
-
-app.use(function(req, res, next) {
-  if (!req.secure ) {
-    console.log({ "Location": "https://" + req.headers['host'] + req.url });
-    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-    // res.redirect (301, 'https://' + req.hostname + ':port' + req.originalUrl);
-  }
-  next();
-});
-
-// httpolyglot.createServer(options, app).listen(80);
-http.createServer(app).listen(80, () => console.log('HTTP Server running on port 80'));
 https.createServer(options, app).listen(443, () => console.log('HTTPS Server running on port 443'));
+*/
 
 
 
