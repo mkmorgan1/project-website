ef00cdb63d3ede7d05d77dbdf16332cd84548cf7
Matthew <m_kmorgan@hotmail.com>
2021-02-09 16:06:10 -0700

    testing connections to test file

--- tests/test.test.js
+++ tests/test.test.js
@@ -2,12 +2,35 @@ import App from '../client/components/App.jsx';
 
 const testFunc = (a, b) => {
   return a + b;
-}
+};
+const testMoney = (a, b, callback) => {
+  return '$' + callback(a, b);
+};
 
-test('adds two numbers', () => {
-  expect(testFunc(1, 2)).toBe(3);
+describe('adds two numbers', () => {
+  test('1, 2 => 3', () => {
+    expect(testFunc(1, 2)).toBe(3);
+  });
+  test('1, 3 => 4', () => {
+    expect(testFunc(1, 3)).toBe(4);
+  });
 });
 
-test('multiplies two numbers', () => {
-  expect(testFunc(1, 2)).toBe(2);
+describe('money', () => {
+  test('1, 2 => $3', () => {
+    const testFunc = jest.fn(() => 3);
+    expect(testMoney(1, 2, testFunc)).toBe('$3');
+  });
+  test('1, 3 => $4', () => {
+    const testFunc = jest.fn(() => 4);
+    expect(testMoney(1, 3, testFunc)).toBe('$4');
+  });
 });
+
+describe('<App/>', () => {
+  test('<App/>', () => {
+    expect(testFunc(1,2)).toBe(3);
+  });
+})
+
+// describe(
