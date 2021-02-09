54456203ed154dfbb441b788dffcdc7929d5797e
Matthew <m_kmorgan@hotmail.com>
2021-02-09 14:25:57 -0700

    created a mock css module

--- tests/test.test.js
+++ tests/test.test.js
@@ -1,7 +1,13 @@
+import App from '../client/components/App.jsx';
+
 const testFunc = (a, b) => {
   return a + b;
 }
 
 test('adds two numbers', () => {
   expect(testFunc(1, 2)).toBe(3);
+});
+
+test('multiplies two numbers', () => {
+  expect(testFunc(1, 2)).toBe(2);
 });
