0b752d896014510482453175ce06e789ad994e1e
Matthew <m_kmorgan@hotmail.com>
2021-02-09 17:46:40 -0700

    Created a crash test for each module

--- tests/test.test.js
+++ tests/test.test.js
@@ -1,36 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from '../client/components/App.jsx';
+import Contact from '../client/components/Contact.jsx';
+import Links from '../client/components/Links.jsx';
+import Titles from '../client/components/Titles.jsx';
+import AboutMe from '../client/components/projects/AboutMe.jsx';
+import HorizontallyScaling from '../client/components/projects/HorizontallyScaling.jsx';
+import RatingsAndReviews from '../client/components/projects/RatingsAndReviews.jsx';
+import TimeTravelAgent from '../client/components/projects/TimeTravelAgent.jsx';
+import * as redux from 'react-redux'
 
-const testFunc = (a, b) => {
-  return a + b;
-};
-const testMoney = (a, b, callback) => {
-  return '$' + callback(a, b);
-};
-
-describe('adds two numbers', () => {
-  test('1, 2 => 3', () => {
-    expect(testFunc(1, 2)).toBe(3);
-  });
-  test('1, 3 => 4', () => {
-    expect(testFunc(1, 3)).toBe(4);
-  });
+/*  HANDLES REACT ERRORS WITH MUTED VIDEOS IN JEST  */
+Object.defineProperty(HTMLMediaElement.prototype, 'muted', {
+  set: () => {},
 });
 
-describe('money', () => {
-  test('1, 2 => $3', () => {
-    const testFunc = jest.fn(() => 3);
-    expect(testMoney(1, 2, testFunc)).toBe('$3');
+
+const Modules = {
+  App: App,
+  Contact: Contact,
+  Link: Links,
+  Titles: Titles,
+  AboutMe: AboutMe,
+  HorizontallyScaling: HorizontallyScaling,
+  RatingsAndReviews: RatingsAndReviews,
+  TimeTravelAgent: TimeTravelAgent,
+}
+const useSelectorMock = jest.spyOn(redux, 'useSelector')
+.mockReturnValue({
+    first: false,
+    second: false,
+    third: false,
+    fourth: false,
   });
-  test('1, 3 => $4', () => {
-    const testFunc = jest.fn(() => 4);
-    expect(testMoney(1, 3, testFunc)).toBe('$4');
-  });
+const useDispatchMock = jest.spyOn(redux, 'useDispatch')
+  .mockReturnValue(jest.fn())
+
+/* NOT SURE IF I NEED THIS 👇🏼 */
+// beforeEach(() => {
+//   useSelectorMock.mockClear()
+//   useDispatchMock.mockClear()
+// })
+
+describe('Should render without crashing', () => {
+  for (let key in Modules) {
+    test(`<${key} />`, () => {
+      const Module = Modules[key];
+      ReactDOM.render(<Module />, document.createElement('div'));
+    });
+  }
 });
-
-describe('<App/>', () => {
-  test('<App/>', () => {
-    expect(testFunc(1,2)).toBe(3);
-  });
-})
-
-// describe(
